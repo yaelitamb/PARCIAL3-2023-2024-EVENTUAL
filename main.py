a58add13ae501e8d80da8279ad4e087c66c68314
yaelitamb <yaelmartin@uma.es>
2025-01-19 19:30:35 +0100

    arreglo

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Cambia "*" por ["http://localhost:3000"] en producción
+    allow_origins=["*"],  # Cambia "*" por ["http://localhost:3000"] en producción
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
