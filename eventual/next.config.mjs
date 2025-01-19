5b5736066e551087a51eac6d25a85cef3e8f86a3
yaelitamb <yaelmartin@uma.es>
2025-01-19 23:09:54 +0100

    Update next.config.mjs

--- eventual/next.config.mjs
+++ eventual/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true, // Activa React Strict Mode para mejores errores y advertencias.
+    experimental: {
+      appDir: true, // Activa la característica de `app/` para rutas.
+    },
+    output: "standalone", // Requerido para Vercel en configuraciones avanzadas.
+  };
+  
+  export default nextConfig;
+  
