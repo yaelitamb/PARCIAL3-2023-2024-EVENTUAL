c4d6252e2832851f4177aacd8feac2eefa224195
yaelitamb <yaelmartin@uma.es>
2025-01-19 20:49:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -97,4 +97,4 @@ async def borrar_evento(evento_id: str):
 if __name__ == "__main__":
     import uvicorn
     port = int(os.getenv("PORT", 8000))  # Usar el puerto proporcionado por Railway
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="127.0.0.1", port=port)
