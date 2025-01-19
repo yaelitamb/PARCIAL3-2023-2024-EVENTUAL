7abc1204e6c425ddd804cbd1a315e1088ae26952
yaelitamb <yaelmartin@uma.es>
2025-01-19 23:37:39 +0100

    Update next.config.mjs

--- eventual/next.config.mjs
+++ eventual/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      // Elimina 'appDir' si está presente
+    },
+    reactStrictMode: true, // Si no está, inclúyelo
+    swcMinify: true,       // Si no está, inclúyelo
+    eslint: {
+        ignoreDuringBuilds: true, // Ignora ESLint durante la construcción
+      }
+  };
+  
+  export default nextConfig;
+  
